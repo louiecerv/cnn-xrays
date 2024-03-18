87ca5303f62d3120e83b31e807abd1f2c464b748
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-18 01:57:52 +0000

    Save

--- Chest_Xrays.py
+++ Chest_Xrays.py
@@ -8,7 +8,7 @@ import time
 def app():
 
 
-    text = """Convolutional Neural Network Image Classifier on the Chest X-Rays Dataset"""
+    text = """Convolutional Neural Network Image Classifier Using Tensorflow and Keras on the Chest X-Rays Dataset"""
     st.subheader(text)
 
     text = """Louie F. Cervantes, M. Eng. (Information Engineering) \n
@@ -21,7 +21,27 @@ def app():
     st.image('chest-xray.jpg', caption='The Chest X-ray Dataset')
 
     text = """
-    \nDescribe this Streamlit App
+    \nTensorFlow and Keras are a powerful combination for building 
+    Convolutional Neural Networks (CNNs) for image classification tasks. 
+    \nTensorFlow:
+    TensorFlow is a powerful open-source library for numerical computation and 
+    machine learning. It provides the core building blocks for creating 
+    and training machine learning models.
+    In CNNs for image classification, TensorFlow handles the low-level
+    operations like:Tensor manipulation (tensors are multidimensional 
+    arrays, the basic unit of data in TensorFlow)
+    Mathematical computations
+    Device optimization (utilizing GPUs for faster training)
+    \nKeras:
+    Keras is a high-level API built on top of TensorFlow, designed to 
+    simplify model building and experimentation.It provides pre-built 
+    layers and functions commonly used in deep learning models, 
+    including CNNs. With Keras, you can define the architecture of your 
+    CNN using a user-friendly interface. This includes specifying layers like:
+    Convolutional layers (extract features from images)
+    Pooling layers (reduce image dimensionality)
+    Activation layers (introduce non-linearity)
+    Fully connected layers (classify the extracted features)
     """
     st.write(text)
 
